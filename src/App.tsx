a9d6f3a8201898933644337e4664961ac38b328d
melvinf21 <melvinfontanez@gmail.com>
2023-07-14 12:53:28 -0400

    adds testing features

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='mx-auto w-96'>
-        <h1 className='text-lg'></h1>Airportle 2.0
-      </div>
-    </>
+    <div className="mx-auto w-96">
+      <h1 className="text-4xl text-center">Airportle 2.0</h1>
+    </div>
   )
 }
 
