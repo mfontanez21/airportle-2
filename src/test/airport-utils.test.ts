64a38d48a8a502d2581ae9e0c5c7a8f29eae6a07
melvinf21 <melvinfontanez@gmail.com>
2023-07-17 15:39:05 -0400

    adds tests for partial miss, partial match

--- src/test/airport-utils.test.ts
+++ src/test/airport-utils.test.ts
@@ -1,10 +1,44 @@
 import { describe, expect, it } from "vitest";
-import { getRandomAirport } from "./airport-utils";
+import { LetterState, getRandomAirport, computeGuess } from "./airport-utils";
 
 
 
 describe('airport-utils', () => {
   it('random airport',  () => {
     expect(getRandomAirport()).toBeTruthy()
+  });
+});
+
+describe('computeGuess', () => {
+  it('works with match and present',  () => {
+    expect(computeGuess('BSL', "BOS")).toEqual([
+      LetterState.Match,
+      LetterState.Present,
+      LetterState.Miss
+    ])
+  });
+
+  it('works with all matches',  () => {
+    expect(computeGuess('BOS', "BOS")).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+    ])
+  });
+
+  it('works with full miss',  () => {
+    expect(computeGuess('BOS', "JFK")).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ])
+  });
+
+  it('only does one match when two letters are present',  () => {
+    expect(computeGuess('TLH', "FLL")).toEqual([
+      LetterState.Miss,
+      LetterState.Match,
+      LetterState.Miss,
+    ])
   });
 });
--- src/test/airport-utils.ts
+++ src/test/airport-utils.ts
@@ -5,3 +5,25 @@ export function getRandomAirport(){
   return airports[randomIndex]
 }
 
+export enum LetterState {
+  Miss,
+  Present,
+  Match,
+}
+
+export function computeGuess(guess: string, airportString: string): LetterState[] {
+  const result: LetterState[] = []
+  const guessArray = guess.split("")
+  const airportArray = airportString.split("")
+
+  guessArray.forEach((letter, index ) => {
+    if (letter === airportArray[index]){
+      result.push(LetterState.Match)
+    } else if (airportArray.includes(letter)) {
+      result.push(LetterState.Present)
+    } else {
+      result.push(LetterState.Miss)
+    }
+  })
+    return result
+}
