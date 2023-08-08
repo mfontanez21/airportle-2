dbf8b62ce86061f90b487b599d4cc9a70ab9e931
melvinf21 <melvinfontanez@gmail.com>
2023-08-08 15:00:37 -0400

    adds game over screen

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,10 @@ export default function App() {
 
   rows = rows.concat(Array(numberOfGuessesRemaining).fill(""))
 
-
+  const isGameOver = state.guesses.length === GUESS_LENGTH
 
   return (
-    <div className="mx-auto w-96">
+    <div className="mx-auto w-96 relative">
       <header className="border-b border-gray-500 pb-2 my-2">
       <h1 className="text-4xl text-center">Airportle 2.0</h1>
       
@@ -44,7 +44,8 @@ export default function App() {
         type="text" 
         className="w-1/2 p-2 border-2 border-gray-500"
         value={guess} 
-        onChange={onChange} />
+        onChange={onChange} 
+        disabled={isGameOver}/>
       </div>
       </header>
       <main className="grid grid-rows-6 gap-4">
@@ -53,6 +54,14 @@ export default function App() {
         ))}
 
       </main>
+
+      {isGameOver && (
+        <div role="modal" className="absolute bg-white
+        left-0 right-0 top-1/4 p-6 w-3/4 mx-auto rounded border border-gray-500 text-center"
+        >
+          Game Over!
+          </div>
+      )}
     </div>
   )
 }
